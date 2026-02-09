99bf583bf778c3476dc424831ad79361ad5ecb0f
Esdras <esdrasbalves10@gmail.com>
2026-02-09 14:01:07 -0300

    filtering, pagination and searching

---
+++ Core/Specifications/ProductSpecParams.cs
@@ -0,0 +1,47 @@
+using System;
+
+namespace Core.Specifications;
+
+public class ProductSpecParams
+{
+    public string? Sort { get; set; }
+    public int PageIndex { get; set; } = 1;
+
+    private const int MaxPageSize = 50;
+    private int _pageSize = 6;
+    private string? _search;
+    private List<string> _brands = [];
+    private List<string> _types = [];
+
+    public List<string> Brands
+    {
+        get => _brands;
+        set
+        {
+            _brands = value.SelectMany(x => x.Split(',', 
+                StringSplitOptions.RemoveEmptyEntries)).ToList();
+        }
+    }
+
+    public List<string> Types
+    {
+        get => _types;
+        set
+        {
+            _types = value.SelectMany(x => x.Split(',', 
+                StringSplitOptions.RemoveEmptyEntries)).ToList();
+        }
+    }
+
+     public int PageSize
+    {
+        get => _pageSize;
+        set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+    }
+
+      public string Search
+    {
+        get => _search ?? "";
+        set => _search = value.ToLower();
+    }
+}
