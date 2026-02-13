3fea1a8f876441d2a4f429f85b368aaa56bb0ca0
Esdras <esdrasbalves10@gmail.com>
2026-02-13 14:38:48 -0300

    angular basics

---
+++ client/src/app/app.ts
@@ -0,0 +1,28 @@
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Header } from "./layout/header/header";
+import { HttpClient } from '@angular/common/http';
+import { Product } from './shared/models/product';
+import { Pagination } from './shared/models/pagination';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Header],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App implements OnInit {
+  baseUrl = '/api/';
+  private http = inject(HttpClient);
+  protected readonly title = signal('esdras');
+
+  products: Product[] = [];
+
+  ngOnInit(): void {
+    this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
+      next: response => this.products = response.data,
+      error: error => console.log(error),
+      complete: () => console.log('complete')
+    });
+  }
+}
